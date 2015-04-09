a99bedbb3f2aae39624ebef420258caffc833c7e
Katie Fitz <fitzsi28@gmail.com>
2015-04-09 13:05:44 -0500

    sac only test system

---
+++ cart-pend.py
@@ -0,0 +1,101 @@
+import numpy as np
+import trep
+from trep import tx, ty, tz, rx, ry, rz
+import sactrep
+
+# set mass, length, and gravity:
+DT = 4./100.
+M = 0.05 #kg
+L = 0.5 # m
+B = 0.002 # damping
+g = 9.81 #m/s^2
+MAXSTEP = 30.0 #m
+BASEFRAME = "base"
+CONTFRAME = "stylus"
+SIMFRAME = "trep_world"
+MASSFRAME = "pend_mass"
+CARTFRAME = "cart"
+
+# define initial config and velocity
+q0 = np.array([0, 0]) # x = [x_cart, theta]
+dq0 = np.array([0, 0])
+
+# define time parameters:
+#dt = 0.0167
+tf = 20.0
+
+# create system
+system = trep.System()
+# define frames
+frames = [
+    ty('ys', name='y-stylus', kinematic=True),
+    ty('yc',name=CARTFRAME, mass=M), [
+        rx('theta', name="pendShoulder"), [
+            tz(-L, name=MASSFRAME, mass=2*M)]]]
+# add frames to system
+system.import_frames(frames)
+# add gravity potential
+trep.potentials.Gravity(system, (0,0,-g))
+# add a damping force on the cart
+trep.forces.Damping(system, B)
+#add a constraint
+trep.constraints.PointOnPlane(system, 'y-stylus', (0.,1.0,0.), CARTFRAME)
+
+
+#############
+# SAC STUFF #
+#############
+
+def proj_func(x):
+    x[1] = np.fmod(x[1]+np.pi, 2.0*np.pi)
+    if(x[1] < 0):
+        x[1] = x[1]+2.0*np.pi
+    x[1] = x[1] - np.pi
+
+def xdes_func(t, x, xdes):
+    xdes[1] = np.pi
+
+sacsys = sactrep.Sac(system)
+
+sacsys.T = 1.2
+sacsys.lam = -10#5
+sacsys.maxdt = 0.2
+sacsys.ts = DT
+sacsys.usat = [[MAXSTEP, -MAXSTEP]]
+sacsys.calc_tm = DT
+sacsys.u2search = False
+sacsys.Q = np.diag([100,200,100,0,100,0]) # yc,th,ys,ycd,thd,ysd
+sacsys.P = 0*np.diag([0,0,0,0,0,0])
+sacsys.R = 0.3*np.identity(1)
+
+sacsys.set_proj_func(proj_func)
+sacsys.set_xdes_func(xdes_func)
+
+# set initial conditions:
+system.q = q0
+system.dq = dq0
+T = [sacsys.time]
+Q = [system.q]
+# init SAC:
+sacsys.init()
+
+# run loop:
+q = np.hstack((system.q[0], system.dq[0],
+               system.q[1], system.dq[1]))
+u = np.array([system.u])
+T.append(sacsys.time)
+Q.append(system.q)
+while sacsys.time < tf:
+    sacsys.step()
+    q = np.vstack((q, np.hstack((system.q[0], system.dq[0],
+                                 system.q[1], system.dq[1]))))
+    u = np.vstack((u, system.u))
+    T.append(sacsys.time)
+    Q.append(system.q)
+    if np.abs(sacsys.time%1)<DT:
+        print "time = ",sacsys.time
+    
+# Visualize the system in action
+trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
+
+np.savetxt("x_py.csv", q, fmt="%9.6f", delimiter=",")
