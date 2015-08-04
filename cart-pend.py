ef30135433093c893e74000ca4d154b8347e0a4e
Katie Fitz <fitzsi28@gmail.com>
2015-08-04 10:32:18 -0500

    sys reoriented-inverted=0

--- cart-pend.py
+++ cart-pend.py
@@ -11,7 +11,7 @@ M = 0.1 #kg
 L = 1.0 # m
 B = 0.1 # damping
 g = 9.81 #m/s^2
-MAXSTEP = 295.0 #m/s^2
+MAXSTEP = 35.0 #m/s^2
 BASEFRAME = "base"
 CONTFRAME = "stylus"
 SIMFRAME = "trep_world"
@@ -20,8 +20,8 @@ CARTFRAME = "cart"
 
 # define initial config and velocity
 
-q0 = np.array([0.,0., 0.]) # x = [x_cart, theta]
-dq0 = np.array([0., 0., 0.])
+q0 = np.array([3./4.*np.pi,0.]) # x = [x_cart, theta]
+dq0 = np.array([0., 0.])
 
 # define time parameters:
 tf = 15.0
@@ -29,39 +29,34 @@ tf = 15.0
 def build_system():
     sys = trep.System()
     frames = [
-        ty('ys', name='y-stylus', kinematic=True),
-        ty('yc',name=CARTFRAME, mass=M), [
-            rx('theta', name="pendShoulder"), [
-                tz(-L, name=MASSFRAME, mass=M)]]]
+        tx('xc',name=CARTFRAME, kinematic=True), [
+            rz('theta', name="pendShoulder"), [
+                ty(L, name=MASSFRAME, mass=M)]]]
     sys.import_frames(frames)
-    trep.potentials.Gravity(sys, (0,0,-g))
+    trep.potentials.Gravity(sys, (0,-g,0))
     trep.forces.Damping(sys, B)
-    trep.constraints.PointOnPlane(sys, 'y-stylus', (0.,1.0,0.), CARTFRAME)
     return sys
 
 def proj_func(x):
-    x[1] = np.fmod(x[1]+np.pi, 2.0*np.pi)
-    if(x[1] < 0):
-        x[1] = x[1]+2.0*np.pi
-    x[1] = x[1] - np.pi
+    x[0] = np.fmod(x[0]+np.pi, 2.0*np.pi)
+    if(x[0] < 0):
+        x[0] = x[0]+2.0*np.pi
+    x[0] = x[0] - np.pi
 
-def xdes_func(t, x, xdes):
-    xdes[1] = np.pi
 
 def build_sac_control(sys):
     sacsyst = sactrep.Sac(sys)
     sacsyst.T = 0.5
-    sacsyst.lam = -20.0
+    sacsyst.lam = -20
     sacsyst.maxdt = 0.2
     sacsyst.ts = DT
     sacsyst.usat = [[MAXSTEP, -MAXSTEP]]
     sacsyst.calc_tm = DT
     sacsyst.u2search = False
-    sacsyst.Q = np.diag([100,200,100,1,40,1]) # yc,th,ys,ycd,thd,ysd
-    sacsyst.P = np.diag([0,10,0,0,0,0])
+    sacsyst.Q = np.diag([200,10,0,0]) # th, x, thd, xd
+    sacsyst.P = np.diag([0,0,0,0])
     sacsyst.R = 0.3*np.identity(1)
     sacsyst.set_proj_func(proj_func)
-    sacsyst.set_xdes_func(xdes_func)
     return sacsyst
 
 system = build_system()
@@ -77,20 +72,18 @@ sacsys.init()
 # run loop:
 q = np.array((system.q[0], system.dq[0],
                system.q[1], system.dq[1]))
-u = np.array([sacsys.controls, sacsys.t_app[1]-sacsys.t_app[0]])
+u = [sacsys.controls[0]]
 T = [sacsys.time]
 Q = [system.q]
 
 while sacsys.time < tf:
-    tic = time.time()
     sacsys.step()
-    toc = time.time()
     t_app = sacsys.t_app[1]-sacsys.t_app[0]
     xcalc = system.q[0]+(system.dq[0]*t_app) + (0.5*sacsys.controls[0]*t_app*t_app)
     fsac = sacsys.controls[0]*M*-1 #SAC resistive force
     q = np.vstack((q, np.hstack((system.q[0], system.q[1],
-                                 system.lambda_(), fsac))))
-    u = np.vstack((u, np.hstack([sacsys.controls, t_app])))
+                                 system.dq[0], system.dq[1]))))
+    u.append(sacsys.controls[0])
     T.append(sacsys.time)
     qtemp = sacsys.q
     proj_func(qtemp)
@@ -98,13 +91,17 @@ while sacsys.time < tf:
     if np.abs(sacsys.time%1)<DT:
         print "time = ",sacsys.time
         
+# Visualize the system in action
+trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
 
-plt.plot(T,Q[0:,:-1])
-plt.plot(T,u[0:,0])
-plt.axis([0,tf,-8,8])
+
+plt.plot(T,Q)
+
+plt.plot(T,u)
+plt.legend(['th','x','u'])
+plt.axis([0,tf,-10,10])
 plt.show()    
 np.savetxt("x_py.csv", q, fmt="%9.6f", delimiter=",")
 np.savetxt("U_py.csv", u, fmt="%9.6f", delimiter=",")
 
-# Visualize the system in action
-trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
+
--- cart-pendLQR.py
+++ cart-pendLQR.py
@@ -22,31 +22,30 @@ MASSFRAME = "pend_mass"
 CARTFRAME = "cart"
 
 
-q0 = 150.*np.pi/180. # Initial configuration of pendulum
+q0 = 30.*np.pi/180. # Initial configuration of pendulum
 t0 = 0.0 # Initial time
 tf = 15.# Final time
 
-system = trep.System() # Initialize system
+def build_system():
+    sys = trep.System()
+    frames = [
+        tx('xc',name=CARTFRAME, kinematic=True), [
+            rz('theta', name="pendShoulder"), [
+                ty(L, name=MASSFRAME, mass=M)]]]
+    sys.import_frames(frames)
+    trep.potentials.Gravity(sys, (0,-g,0))
+    trep.forces.Damping(sys, B)
+    return sys
 
-frames = [
-    ty('ys', name='y-stylus', kinematic=True),
-    ty('yc',name=CARTFRAME, mass=M), [ 
-        rx('theta', name="pendulumShoulder"), [
-            tz(-L, name=MASSFRAME, mass=M)]]]
-system.import_frames(frames) # Add frames
-
-# Add forces to the system
-trep.potentials.Gravity(system, (0, 0, -g)) # Add gravity
-trep.forces.Damping(system, B) # Add damping
-trep.constraints.PointOnPlane(system, 'y-stylus', (0.,1.0,0.), CARTFRAME)#add a constraint
+system = build_system()
 
 
 # Create and initialize the variational integrator
 mvi = trep.MidpointVI(system)
 mvi.initialize_from_configs(t0, np.array([q0]), t0+DT, np.array([q0]))
 
-qBar = np.array([0., -np.pi, 0.]) # Desired configuration
-Q = np.diag([0.5,1,0.5,0.5,1,0.5]) # Cost weights for states
+qBar = np.array([0.,0.]) # Desired configuration
+Q = np.diag([1,0,1,1]) # Cost weights for states
 R = 0.1*np.eye(1) # Cost weights for inputs
 # Create discrete system
 TVec = np.arange(t0, tf+DT, DT) # Initialize discrete time vector
@@ -56,12 +55,11 @@ xBar = dsys.build_state(Q=qBar,p = np.zeros(system.nQd)) # Create desired state 
 # Design linear feedback controller
 Qd = np.zeros((len(TVec), dsys.system.nQ)) # Initialize desired configuration trajectory
 thetaIndex = dsys.system.get_config('theta').index # Find index of theta config variable
-ycIndex = dsys.system.get_config('yc').index
-ysIndex = dsys.system.get_config('ys').index
+xcIndex = dsys.system.get_config('xc').index
+
 for i,t in enumerate(TVec):
-    Qd[i, thetaIndex] = qBar[1] # Set desired configuration trajectory
-    Qd[i, ycIndex] = qBar[0]
-    Qd[i, ysIndex] = qBar[2]
+    Qd[i, thetaIndex] = qBar[0] # Set desired configuration trajectory
+    Qd[i, xcIndex] = qBar[1]
     (Xd, Ud) = dsys.build_trajectory(Qd) # Set desired state and input trajectory
 
 Qk = lambda k: Q # Create lambda function for state cost weights
@@ -70,7 +68,7 @@ KVec = dsys.calc_feedback_controller(Xd, Ud, Qk, Rk) # Solve for linear feedback
 KStabilize = KVec[0] # Use only use first value to approximate infinite-horizon optimal controller gain
 
 # Reset discrete system state
-dsys.set(np.array([0.,q0, 0.,0.,0.,0.]), np.array([0.]), 0)
+dsys.set(np.array([q0, 0.,0.,0.]), np.array([0.]), 0)
 
 # Simulate the system forward
 T = [mvi.t1] # List to hold time values
@@ -99,7 +97,7 @@ ax1 = pylab.subplot(211)
 pylab.plot(T, X)
 pylab.title("Linear Feedback Controller")
 pylab.ylabel("X")
-pylab.legend(["x","theta","x","dx","dtheta","dx"])
+pylab.legend(["theta","x","dtheta","dx"])
 pylab.subplot(212, sharex=ax1)
 pylab.plot(T[1:], U)
 pylab.xlabel("T")
